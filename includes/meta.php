51da1367827c80a0ad64595961d9628dfe83de29
D14RAP <D14RAP@users.noreply.github.com>
2018-03-14 20:09:50 +0000

    🚀

---
+++ includes/meta.php
@@ -0,0 +1,37 @@
+<title><?=$var["title"]?></title>
+        <meta charset="utf-8">
+        <base href="<?=$var["url"]?>" target="_self">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" integrity="sha256-j+P6EZJVrbXgwSR5Mx+eCS6FvP9Wq27MBRC/ogVriY0=" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#000000">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="author" content="<?=$var["author"]?>">
+        <meta name="description" content="<?=$var["description"]?>">
+        <meta name="image" content="<?=$var["image_url"]?>">
+        <meta itemprop="name" content="<?=$var["title"]?>">
+        <meta itemprop="description" content="<?=$var["description"]?>">
+        <meta itemprop="image" content="<?=$var["image_url"]?>">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="<?=$var["title"]?>">
+        <meta name="twitter:description" content="<?=$var["description"]?>">
+        <meta name="twitter:site" content="<?=$var["handle"]?>">
+        <meta name="twitter:creator" content="<?=$var["handle"]?>">
+        <meta name="twitter:image:src" content="<?=$var["twitter_image_url"]?>">
+        <meta name="twitter:player" content="<?=$var["twitter_video_url"]?>">
+        <meta name="og:title" content="<?=$var["title"]?>">
+        <meta name="og:description" content="<?=$var["description"]?>">
+        <meta name="og:image" content="<?=$var["image_url"]?>">
+        <meta name="og:url" content="<?=$var["url"]?>">
+        <meta name="og:site_name" content="<?=$var["title"]?>">
+        <meta name="og:locale" content="<?=$var["locale"]?>">
+        <meta name="og:video" content="<?=$var["video_url"]?>">
+        <meta name="fb:admins" content="<?=$var["facebook_admin_id"]?>">
+        <meta name="fb:app_id" content="<?=$var["facebook_app_id"]?>">
+        <meta name="og:type" content="website">
+        <!--<script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>-->
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php $var = parse_ini_file("variables.ini"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("includes/meta.php"); ?>
+    </head>
+    <body>
+        <?php include("includes/scripts.php"); ?>
+    </body>
+</html>
