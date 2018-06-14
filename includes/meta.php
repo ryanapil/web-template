a1c6d167e5331ef49dd76f1ffe883716c5c310af
ryanapil <hello@ryanapil.uk>
2018-06-14 21:21:05 +0100

    fix

--- includes/meta.php
+++ includes/meta.php
@@ -2,7 +2,8 @@
         <meta charset="utf-8">
         <base href="<?=$var["url"]?>" target="_self">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">-->
+        <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">-->
         <link rel="stylesheet" href="css/main.css?<?=$var["version"]?>">
         <meta name="author" content="<?=$var["author"]?>">
         <meta name="description" content="<?=$var["description"]?>">
@@ -27,4 +28,3 @@
         <meta name="fb:admins" content="<?=$var["facebook_admin_id"]?>">
         <meta name="fb:app_id" content="<?=$var["facebook_app_id"]?>">
         <meta name="og:type" content="website">
-        <!--<script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>-->
